6e0e53c5ea629af08eab405fc1dcb87f3a81cdd4
MatheusPMC <matheus.pmc@hotmail.com>
2021-02-12 21:15:02 -0300

    Cliente, TipoCliente, telefones e enderecos

---
+++ src/main/java/com/matheusprado/cursomc/repositories/EnderecoRepository.java
@@ -0,0 +1,11 @@
+package com.matheusprado.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.matheusprado.cursomc.domain.Endereco;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Integer> {
+
+}
