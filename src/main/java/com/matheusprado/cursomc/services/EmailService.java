84e5484741b9f879fd1e432f83d73c8258c0de12
MatheusPMC <matheus.pmc@hotmail.com>
2021-02-16 19:44:51 -0300

    Email HTML

--- src/main/java/com/matheusprado/cursomc/services/EmailService.java
+++ src/main/java/com/matheusprado/cursomc/services/EmailService.java
@@ -1,5 +1,7 @@
 package com.matheusprado.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.matheusprado.cursomc.domain.Pedido;
@@ -9,4 +11,8 @@ public interface EmailService {
 	void sendOrderConfimationEmail(Pedido obj);
 	
 	void sendEmail(SimpleMailMessage msg);
+	
+	void sendOrderConfimationHtmlEmail(Pedido obj);
+	
+	void sendHtmlEmail(MimeMessage msg);
 }
